69a6299ccf8a72b416bb888f0cfd0e284915cd22
Ilya Anosov <anosovilya465@yandex.ru>
2021-01-25 06:42:25 +0900

    Added WindowIcon.png Added BackgroundImage.jpg Updated main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ N) .
 import os
 import random
 import sqlite3
-import time
+import sys
 
 import pygame
 
@@ -23,21 +23,44 @@ __maintainer__ = ["Ilya B. Anosov"]
 __email__ = "anosovilya465@yandex.ru"
 __status__ = "Development"
 
+BACKGROUND_POSITION = (0, 0)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
+    sys.excepthook = except_hook
+
     # инициализация Pygame:
     pygame.init()
+
     # размеры окна:
     size = width, height = 800, 600
+
     # screen — холст, на котором нужно рисовать:
     screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption('Tanks. Shoot-out')
+    icon = pygame.image.load('data/WindowIcon.png')
+    pygame.display.set_icon(icon)
+
+    background_image = pygame.image.load("data/BackgroundImage.jpg").convert()
+    background_image = pygame.transform.scale(background_image, size)
+    screen.blit(background_image, BACKGROUND_POSITION)
+
     # формирование кадра:
     # команды рисования на холсте
     # ...
     # ...
+
     # смена (отрисовка) кадра:
     pygame.display.flip()
+
     # ожидание закрытия окна:
     while pygame.event.wait().type != pygame.QUIT:
         pass
+
     # завершение работы:
     pygame.quit()
